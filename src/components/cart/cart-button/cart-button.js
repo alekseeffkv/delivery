a9c9f1f5311bd5a3daf9095e6ecb6812bb3446d7
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-01 11:28:32 +0300

    add cart-button

---
+++ src/components/cart/cart-button/cart-button.js
@@ -0,0 +1,17 @@
+import { ReactComponent as CartIcon } from '../../../icons/cart.svg';
+import './cart-button.scss';
+
+const CartButton = () => {
+  return (
+    <div className="cart-button">
+      <div className="cart-button__title">Корзина</div>
+      <div className="cart-button__counter">
+        <div className="cart-button__circle">
+          <div className="cart-button__amount">4</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartButton;
