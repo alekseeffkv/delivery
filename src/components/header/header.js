a59662bb6e06d565d73ea50818e41e68e4c9a87d
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-01 11:28:50 +0300

    add header

---
+++ src/components/header/header.js
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom';
+import './header.scss';
+
+import Search from '../search';
+import CartButton from '../cart/cart-button';
+
+import { ReactComponent as PhoneIcon } from '../../icons/phone.svg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__container">
+        <Link to="/">
+          <span className="header__logo">DELIVERY</span>
+        </Link>
+
+        <Search />
+
+        <div className="header__contacts">
+          <PhoneIcon />
+          <div className="header__contacts-inner">
+            <div className="header__contacts-title">Контакты:</div>
+            <div className="header__contacts-number">+7 (917) 510-57-59</div>
+          </div>
+        </div>
+
+        <CartButton />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
