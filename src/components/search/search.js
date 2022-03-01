da2050bb26d134fdab405509671e5f459727b2dc
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-01 11:27:47 +0300

    add search

---
+++ src/components/search/search.js
@@ -0,0 +1,15 @@
+import './search.scss';
+import { ReactComponent as SearchIcon } from '../../icons/search.svg';
+
+const Search = () => {
+  return (
+    <div className="search">
+      <input type="search" placeholder="Введите название блюда" className="search__input" />
+      <button type="button" className="search__button">
+        <SearchIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Search;
