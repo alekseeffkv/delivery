0624225f89fecef814d1e50ac6396f27177e7cca
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-24 13:35:42 +0300

    added component Button

---
+++ src/components/button/button.js
@@ -0,0 +1,29 @@
+import cn from 'classnames';
+import './button.scss';
+
+import { ReactComponent as PlusIcon } from '../../icons/plus.svg';
+import { ReactComponent as MinusIcon } from '../../icons/minus.svg';
+import { ReactComponent as CartIcon } from '../../icons/cart.svg';
+
+const icons = {
+  plus: PlusIcon,
+  minus: MinusIcon,
+  cart: CartIcon,
+};
+
+const Button = ({ title, icon, small = false, ...props }) => {
+  const Icon = icons[icon];
+  return (
+    <button
+      className={cn('button', {
+        button_small: small,
+      })}
+      {...props}
+    >
+      {title && <div className="button__title">{title}</div>}
+      {Icon && <Icon />}
+    </button>
+  );
+};
+
+export default Button;
