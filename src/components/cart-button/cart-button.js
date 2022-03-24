495b15bbae1081f7ac00df8675a4dfb1e6abbec1
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-24 13:34:18 +0300

    fix Cart-button

--- src/components/cart-button/cart-button.js
+++ src/components/cart-button/cart-button.js
@@ -4,14 +4,14 @@ import './cart-button.scss';
 const CartButton = () => {
   return (
     <CSSTransition in={true} appear timeout={500} classNames="cart-button">
-      <div className="cart-button">
+      <button className="cart-button">
         <div className="cart-button__title">Корзина</div>
         <div className="cart-button__counter">
           <div className="cart-button__circle">
             <div className="cart-button__amount">4</div>
           </div>
         </div>
-      </div>
+      </button>
     </CSSTransition>
   );
 };
