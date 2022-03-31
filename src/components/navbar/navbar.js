bdb042b966a5fd9dfdfd272dbda22912030458cf
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-03-31 09:04:48 +0300

    added NavLink

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -2,8 +2,8 @@ import './navbar.scss';
 
 import { useState, useEffect } from 'react';
 import { CSSTransition } from 'react-transition-group';
+import { NavLink } from 'react-router-dom';
 
-import NavItem from '../nav-item';
 import CartButton from '../cart-button';
 
 const Navbar = ({ categories }) => {
@@ -20,12 +20,26 @@ const Navbar = ({ categories }) => {
     return () => window.removeEventListener('scroll', handleScroll);
   }, []);
 
+  const navItems = categories.map(({ id, name }) => ({
+    label: name,
+    url: `/categories/${id}`,
+  }));
+
   return (
     <nav className="navbar">
       <div className="navbar__inner">
-        {categories.map((category) => (
-          <NavItem key={category.id} id={category.id} name={category.name} />
+        {navItems.map(({ label, url }) => (
+          <NavLink
+            to={url}
+            key={label}
+            className="navbar__nav-item"
+            activeClassName="navbar_active"
+            onClick={() => window.scrollTo(0, 550)}
+          >
+            {label}
+          </NavLink>
         ))}
+
         {navbarCart && (
           <CSSTransition in={true} appear timeout={500} classNames="cart-button">
             <CartButton />
