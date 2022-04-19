8ab9b7060d6fe5ae9d349c184fbd0dd572bf58a6
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-04-19 10:13:18 +0300

    Add transition style

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -5,7 +5,7 @@ import { ReactComponent as SearchIcon } from '../../icons/search.svg';
 const Search = () => {
   return (
     <div className="search">
-      <input type="search" placeholder="Введите название блюда" className="search__input" />
+      <input type="search" placeholder=" Введите название блюда" className="search__input" />
       <button type="button" className="search__button">
         <SearchIcon />
       </button>
