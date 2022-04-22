da7e1d388ba341fb488eae775605be0809f8cf22
Konstantin Alekseev <alekseeffkv@gmail.com>
2022-04-22 14:48:32 +0300

    Fix Banner

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -7,8 +7,8 @@ const Banner = () => {
   return (
     <div className="banner">
       <picture className="banner__picture">
-        <source media="(max-width: 480px)" srcSet={mobileBanner} />
-        <source media="(min-width: 481px)" srcSet={banner} />
+        <source media="(max-width: 599px)" srcSet={mobileBanner} />
+        <source media="(min-width: 600px)" srcSet={banner} />
         <img src={banner} className="banner__image" alt="banner" />
       </picture>
     </div>
